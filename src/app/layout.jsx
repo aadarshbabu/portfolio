2e7fdd744e8e51749cdce94fefc4f8e598ecbb6f
aadarsh <aadarshsingh121@gmail.com>
2026-09-22 00:40:21 +0530

    refactor: update and reorganize favicon and web app manifest assets

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -77,21 +77,7 @@ export const metadata = {
 		images: ['/img/aadarshimprove.jpeg'],
 		creator: '@aadarshsingh121',
 	},
-	icons: {
-		icon: [
-			{ url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-			{ url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-		],
-		apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180' }],
-		other: [
-			{
-				rel: 'mask-icon',
-				url: '/favicon/safari-pinned-tab.svg',
-				color: '#5bbad5',
-			},
-		],
-	},
-	manifest: '/favicon/site.webmanifest',
+
 };
 
 export const viewport = {
